1449c79de2a4cb3af94744c11fc21dff458badae
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-08-13 14:28:22 +0700

    [Add] Footer Component

--- talknchip/src/components/Index/Footer.js
+++ talknchip/src/components/Index/Footer.js
@@ -1,7 +1,20 @@
 import React from "react"
+import styled from "styled-components"
 
-const Footer = ()=>(
-
+const creditText = {
+  creditText: "Made with <3 by",
+  creditHilight: "Artisan + Alchemist"
+}
+const Credit = styled.p`
+  padding: 25px 80px;
+  span{
+    color: #f8bc00;
+  }
+`
+const Footer = () => (
+  <div>
+    <Credit>{creditText.creditText} <span><b>{creditText.creditHilight}</b></span></Credit>
+  </div>
 )
 
 export default Footer
