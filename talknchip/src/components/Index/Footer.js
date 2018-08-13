2b45f4be402306f1b95e7e4e76e458df15de5590
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-08-13 16:04:52 +0700

    [ADD] FooterContainer Component

--- talknchip/src/components/Index/Footer.js
+++ talknchip/src/components/Index/Footer.js
@@ -2,6 +2,10 @@ import React from "react"
 import styled from "styled-components"
 import { Container, Row, Col } from "reactstrap"
 
+const FooterContainer = styled(Container)`
+  background-color: #F1F1F1;
+`
+
 const creditText = {
   creditText: "Made with <3 by",
   creditHilight: "Artisan + Alchemist"
@@ -23,19 +27,21 @@ const images = {
 }
 
 const Footer = () => (
-  <Container className='py-1'>
-    <Row>
-      <Col xs={12} lg={{size: 10, offset: 1}}>
-        <div className='d-flex justify-content-between align-items-center'>
-          <Credit>{creditText.creditText} <span><b>{creditText.creditHilight}</b></span></Credit>
-          <div>
-            <CreditImage src={images.itforge} />
-            <CreditImage src={images.alchemist} />
+  <FooterContainer fluid>
+    <Container className='py-1'>
+      <Row>
+        <Col xs={12} lg={{size: 10, offset: 1}}>
+          <div className='d-flex justify-content-between align-items-center'>
+            <Credit>{creditText.creditText} <span><b>{creditText.creditHilight}</b></span></Credit>
+            <div>
+              <CreditImage src={images.itforge} />
+              <CreditImage src={images.alchemist} />
+            </div>
           </div>
-        </div>
-      </Col>
-    </Row>
-  </Container>
+        </Col>
+      </Row>
+    </Container>
+  </FooterContainer>
 )
 
 export default Footer
