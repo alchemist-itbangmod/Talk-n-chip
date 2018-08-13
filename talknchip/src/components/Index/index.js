5d34c336648b136cc84e8eb7ea4a6f875f2ae469
fame <>
2018-08-13 15:04:08 +0700

    [UPDATE] add schedule into indexpage

--- talknchip/src/components/Index/index.js
+++ talknchip/src/components/Index/index.js
@@ -2,11 +2,13 @@ import React, { Fragment } from "react"
 import HeroSection from "./HeroSection"
 import MainSession from "./MainSession"
 import SectionSubmid from "./SectionSubmidTopic"
+import Schedule from "./Schedule"
 
 export default () => (
   <Fragment>
     <HeroSection />
     <MainSession />
+    <Schedule />
     <SectionSubmid />
   </Fragment>
 )
