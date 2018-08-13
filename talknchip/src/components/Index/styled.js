d16b92fdca8048d28a89740449bac11384cc157f
fame <>
2018-08-13 12:47:59 +0700

    [UPDATE] add const Text & Date

--- talknchip/src/components/Index/styled.js
+++ talknchip/src/components/Index/styled.js
@@ -3,4 +3,13 @@ import styled from "styled-components"
 const Landing = styled.div`
   color:#69302c;
 `
-export default Landing
+
+const Text = styled.h1`
+  margin-bottom: 3%;
+  margin-top: 10%;
+  margin-left: 3%;
+`
+const Date = styled.p`
+  margin-left: 3%;
+`
+export {Landing, Text, Date}
