6b7a6e59a2bdba9ab780179c7e8ea5e193c22d75
pea <pepea.23@live.com>
2018-08-13 16:28:36 +0700

    [Add sponsor]

--- talknchip/src/components/Index/index.js
+++ talknchip/src/components/Index/index.js
@@ -5,10 +5,12 @@ import MainSession from "./MainSession"
 import SectionSubmit from "./SectionSubmitTopic"
 import Schedule from "./Schedule"
 import Footer from "./Footer"
+import Sponsor from "./Sponsor"
 
 export default () => (
   <Fragment>
     <HeroSection />
+    <Sponsor />
     <WhatSection />
     <MainSession />
     <Schedule />
