b1aaa2ffda078bf331575243d1895e9c277a22dd
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-08-13 15:44:00 +0700

    [ADD] image in Footer Component

--- talknchip/src/components/Index/Footer.js
+++ talknchip/src/components/Index/Footer.js
@@ -1,20 +1,41 @@
 import React from "react"
 import styled from "styled-components"
+import { Container, Row, Col } from "reactstrap"
 
 const creditText = {
   creditText: "Made with <3 by",
   creditHilight: "Artisan + Alchemist"
 }
 const Credit = styled.p`
-  padding: 25px 80px;
+  margin-bottom: 0px;
   span{
     color: #f8bc00;
   }
 `
+const CreditImage = styled.img`
+  width: auto;
+  max-height: 80px;
+  padding: 10px 30px;
+`
+const images = {
+  itforge: "/static/images/logoItForge.png",
+  alchemist: "/static/images/logoIAlchemist.png"
+}
+
 const Footer = () => (
-  <div>
-    <Credit>{creditText.creditText} <span><b>{creditText.creditHilight}</b></span></Credit>
-  </div>
+  <Container className='py-1'>
+    <Row>
+      <Col xs={12} lg={{size: 10, offset: 1}}>
+        <div className='d-flex justify-content-between align-items-center'>
+          <Credit>{creditText.creditText} <span><b>{creditText.creditHilight}</b></span></Credit>
+          <div>
+            <CreditImage src={images.itforge} />
+            <CreditImage src={images.alchemist} />
+          </div>
+        </div>
+      </Col>
+    </Row>
+  </Container>
 )
 
 export default Footer
