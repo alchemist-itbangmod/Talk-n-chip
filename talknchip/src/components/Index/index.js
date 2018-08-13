f17c75bde07a90201be8e8a28d231222de925345
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-08-13 14:33:10 +0700

    [ADD] Footer Component in Index

--- talknchip/src/components/Index/index.js
+++ talknchip/src/components/Index/index.js
@@ -2,12 +2,14 @@ import React from "react"
 
 import WhatSection from "./WhatSection"
 import MainSession from "./MainSession"
+import Footer from "./Footer"
 
 const IndexPage = () => (
   <div>
     Talk n chip
     <WhatSection />
     <MainSession />
+    <Footer />
   </div>
 )
 
