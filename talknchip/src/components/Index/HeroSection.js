da4355de5577d30fcd5533ec6839e95129e496f4
fame <>
2018-08-13 16:55:00 +0700

    [UPDATE] Update Button file

--- talknchip/src/components/Index/HeroSection.js
+++ talknchip/src/components/Index/HeroSection.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Link from "gatsby-link"
 import { Container, Row, Col } from "reactstrap"
-import Button from "../core/button"
+import Button from "../core/Button"
 import styled from "styled-components"
 import NavBar from "../Core/Navbar.js"
 
