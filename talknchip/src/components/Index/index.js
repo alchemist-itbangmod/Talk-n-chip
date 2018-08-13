2683d4e0ba1f2d8c596ddb4c4a311831f64692fb
pea <pepea.23@live.com>
2018-08-13 12:15:38 +0700

    [Add โคลง Hero section]

--- talknchip/src/components/Index/index.js
+++ talknchip/src/components/Index/index.js
@@ -1,10 +1,26 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from "react"
+import Link from "gatsby-link"
+import { Container, Row, Col } from "reactstrap"
+import Button from "../core/button"
 
 const IndexPage = () => (
-  <div>
-    Talk n chip
-  </div>
+  <Container>
+    <Row>
+      <Col md='2' />
+      <Col md='8'>
+        <h3>ถ้าพูดเยอะจะเจ็บคอแต่ถ้ามา <br /> Talk n' Chip <br />แล้วไม่พูดจะเสียใจ</h3>
+        <p>
+        26 สิงหาคม 2018 ณ คณะไอที ลาดกระบัง
+        </p>
+        <Button>
+        ส่งหัวข้อที่จะไปพูดกันเลย !
+        </Button>
+        <Link to='/404'> โหวตหัวข้อตัวเองที่อยากฟัง
+        </Link>
+      </Col>
+      <Col md='2' />
+    </Row>
+  </Container>
 )
 
 export default IndexPage
