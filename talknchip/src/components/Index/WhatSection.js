871873e4f111f2bfcccc240006e87d72a081b356
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-08-13 13:08:40 +0700

    [UPDATE] styled WhatSection Component

--- talknchip/src/components/Index/WhatSection.js
+++ talknchip/src/components/Index/WhatSection.js
@@ -1,4 +1,5 @@
 import React from "react"
+import styled from "styled-components"
 import { Container, Row, Col } from "reactstrap"
 
 const content = {
@@ -12,17 +13,40 @@ const content = {
   `
 }
 
+const WhatHeader = styled.h1`
+  color: #69302c;
+
+  span {
+    color: #f8bc00;
+  }
+`
+const WhatP = styled.p`
+  margin : 40px;
+`
+
+const WhatImage = styled.img`
+  max-width: 160px;
+  border-radius: 50%;
+  padding: 10px 30px;
+`
+
+const images = [
+  "https://pbs.twimg.com/profile_images/1011171409471524864/vDjaHTj8_400x400.jpg",
+  "https://pbs.twimg.com/profile_images/1011171409471524864/vDjaHTj8_400x400.jpg",
+  "https://pbs.twimg.com/profile_images/1011171409471524864/vDjaHTj8_400x400.jpg"
+]
+
 const WhatSection = () => (
   <Container>
     <Row>
       <Col className='text-center' xs={12} lg={{size: 10, offset: 1}}>
-        <h1>{content.header}<span>{content.headerHighlight}</span></h1>
-        <p>{content.body}</p>
-      </Col>
-      <Col>
-        <img />
-        <img />
-        <img />
+        <WhatHeader>{content.header} <span><b>{content.headerHighlight}</b></span></WhatHeader>
+        <WhatP>{content.body}</WhatP>
+        {
+          images.map((image, index) => (
+            <WhatImage src={image} key={index} />
+          ))
+        }
       </Col>
     </Row>
   </Container>
