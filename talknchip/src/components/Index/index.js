db1c6e8f3804766af89d692719cf41a2874a5b98
pea <pepea.23@live.com>
2018-08-13 12:33:37 +0700

    [Add Styled button]

--- talknchip/src/components/Index/index.js
+++ talknchip/src/components/Index/index.js
@@ -2,25 +2,37 @@ import React from "react"
 import Link from "gatsby-link"
 import { Container, Row, Col } from "reactstrap"
 import Button from "../Core/button"
+import Landing from "./styled"
+import styled from "styled-components"
+
+const LinkTo = styled(Link)`
+  color: #69302c;
+  &:hover{
+    color: #69302c;
+  }
+`
 
 const IndexPage = () => (
-  <Container>
-    <Row>
-      <Col md='2' />
-      <Col md='8'>
-        <h3>ถ้าพูดเยอะจะเจ็บคอแต่ถ้ามา <br /> Talk n' Chip <br />แล้วไม่พูดจะเสียใจ</h3>
-        <p>
+  <Landing>
+    <Container>
+      <Row>
+        <Col md='2' />
+        <Col md='8'>
+          <h3>ถ้าพูดเยอะจะเจ็บคอแต่ถ้ามา <br /> Talk n' Chip <br />แล้วไม่พูดจะเสียใจ</h3>
+          <p>
         26 สิงหาคม 2018 ณ คณะไอที ลาดกระบัง
-        </p>
-        <Button>
+          </p>
+          <Button>
         ส่งหัวข้อที่จะไปพูดกันเลย !
-        </Button>
-        <Link to='/404'> โหวตหัวข้อตัวเองที่อยากฟัง
-        </Link>
-      </Col>
-      <Col md='2' />
-    </Row>
-  </Container>
+          </Button>
+            หรือ
+          <LinkTo to='/404'> โหวตหัวข้อตัวเองที่อยากฟัง >
+          </LinkTo>
+        </Col>
+        <Col md='2' />
+      </Row>
+    </Container>
+  </Landing>
 )
 
 export default IndexPage
--- talknchip/src/components/Index/styled.js
+++ talknchip/src/components/Index/styled.js
@@ -2,4 +2,8 @@ import styled from "styled-components"
 
 const Landing = styled.div`
 color:#69302c;
+magin:10%;
+
+
 `
+export default Landing
