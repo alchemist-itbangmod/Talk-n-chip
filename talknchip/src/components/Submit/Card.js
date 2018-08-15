54a2085715de343f38d1b61b6bddbdbd3eb57dfb
pea <pepea.23@live.com>
2018-08-15 22:51:36 +0700

    [Add  card]

---
+++ talknchip/src/components/Submit/Card.js
@@ -0,0 +1,86 @@
+import React, { Fragment } from "react"
+import styled from "styled-components"
+import { Card, CardBody,
+  CardTitle, CardSubtitle, Container, Row, Col } from "reactstrap"
+
+const speakers = [
+  {
+    date: "เสนอเมื่อ 14/8",
+    topic: "ทอดหมูกรอบไม่ให้กรอบ ถ้าจะให้กรอบต้องทำให้"
+  },
+  {
+    date: "เสนอเมื่อ 14/8",
+    topic: "ทอดหมูกรอบไม่ให้กรอบ ถ้าจะให้กรอบต้องทำให้"
+  },
+  {
+    date: "เสนอเมื่อ 14/8",
+    topic: "ทอดหมูกรอบไม่ให้กรอบ ถ้าจะให้กรอบต้องทำให้"
+  },
+  {
+    date: "เสนอเมื่อ 14/8",
+    topic: "ทอดหมูกรอบไม่ให้กรอบ ถ้าจะให้กรอบต้องทำให้"
+  },
+  {
+    date: "เสนอเมื่อ 14/8",
+    topic: "ทอดหมูกรอบไม่ให้กรอบ ถ้าจะให้กรอบต้องทำให้"
+  }
+]
+
+const SessionContainer = styled(Container)`
+  background-color: #F1BE42;
+  color: #64342F;
+`
+
+const InnerSessionContainer = styled(Container)`
+  h1 {
+    font-weight: 500;
+  }
+  span {
+    font-weight: 700;
+  }
+  p {
+    font-size: 1.75rem;
+  }
+`
+const CARDBody = styled(CardBody)`
+color:#69302c;
+
+`
+
+const SpeakerContainer = () => (
+  <Fragment>
+    {
+      speakers.map((speaker, index) => (
+        <Col key={index} xs={12} lg={6} className='pb-5' >
+          <CardSubmit speaker={speaker} />
+        </Col>
+      ))
+    }
+  </Fragment>
+)
+
+const CardSubmit = ({ speaker: {topic, date} }) => (
+
+  <Card >
+    <CARDBody className='text-center'>
+      <CardTitle>{topic}</CardTitle>
+      <CardSubtitle>{date}</CardSubtitle>
+    </CARDBody>
+  </Card>
+
+)
+const MainSession = () => (
+  <SessionContainer fluid className='px-0 pt-5'>
+    <InnerSessionContainer>
+      <Row>
+        <Col >
+          <Row>
+            <SpeakerContainer />
+          </Row>
+        </Col>
+      </Row>
+    </InnerSessionContainer>
+  </SessionContainer>
+)
+
+export default MainSession
--- talknchip/src/components/Submit/index.js
+++ talknchip/src/components/Submit/index.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react'
 import Header from './LoginSection'
+import Card from './Card'
 
 class SubmitPage extends React.Component {
   render () {
     return (
       <Fragment>
         <Header/>
+        <Card/>
       </Fragment>
     )
   }
