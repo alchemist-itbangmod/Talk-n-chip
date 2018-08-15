33d3b5a1a8d80f6b829a97890d63b84fbd6a48a9
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-08-15 22:19:54 +0700

    [ADD] SubmitTopic and Footer Component

--- talknchip/src/components/Submit/index.js
+++ talknchip/src/components/Submit/index.js
@@ -1,10 +1,14 @@
 import React from "react"
+import Footer from "../Index/Footer"
+import SubmitTopic from "./SubmitTopic";
 
 class SubmitPage extends React.Component {
   render () {
     return (
       <div >
         Test ja
+        <SubmitTopic />
+        <Footer />
       </div>
     )
   }
