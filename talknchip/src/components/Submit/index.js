d7d464fe0a73c7ca6de3f94c0dc546c46740edd7
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-08-15 23:16:21 +0700

    [ADD] SubmitTopic Component

--- talknchip/src/components/Submit/index.js
+++ talknchip/src/components/Submit/index.js
@@ -1,18 +1,18 @@
-import React, { Fragment } from 'react'
-import Header from './LoginSection'
-import Card from './Card'
-import SubmitTopic from "./SubmitTopic";
+import React, { Fragment } from "react"
+import Header from "./LoginSection"
+import Card from "./Card"
+import SubmitTopic from "./SubmitTopic"
 import Footer from "../Index/Footer"
 
 class SubmitPage extends React.Component {
   render () {
     return (
       <Fragment>
-        <Header/>
-        <Card/>
+        <Header />
+        <SubmitTopic />
+        <Card />
         <Footer />
       </Fragment>
-       
     )
   }
 }
