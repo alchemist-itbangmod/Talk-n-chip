3e37345dcef18a0233772226050708addd867f89
fame <>
2018-08-18 16:45:50 +0700

    [UPDATE] change import modals

--- talknchip/src/components/Submit/SubmitTopic.js
+++ talknchip/src/components/Submit/SubmitTopic.js
@@ -1,7 +1,7 @@
 import React from "react"
 import styled from "styled-components"
 import { Container, Row, Col } from "reactstrap"
-import Modals from "../core/Modals"
+import Modals from "./Modals"
 
 const topic = "หัวข้อที่เสนอ"
 
