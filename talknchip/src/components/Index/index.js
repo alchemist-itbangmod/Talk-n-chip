f6406a0c0164e1e9d269af08d083e2d7749a41ab
fame <>
2018-08-19 20:53:22 +0700

    [UPDATE] refactor

--- talknchip/src/components/Index/index.js
+++ talknchip/src/components/Index/index.js
@@ -6,30 +6,27 @@ import SectionSubmit from "./SectionSubmitTopic"
 import Schedule from "./Schedule"
 import Footer from "./Footer"
 import Sponsor from "./Sponsor"
-import Scroll from "../core/Scroll"
 
 export default () => (
   <Fragment>
-    <Scroll>
-      <section id='home' name='home'>
-        <HeroSection />
-      </section>
-      <section>
-        <Sponsor />
-      </section>
-      <section id='what' name='what'>
-        <WhatSection />
-      </section>
-      <section id='who' name='who'>
-        <MainSession />
-      </section>
-      <section id='where' name='where'>
-        <Schedule />
-      </section>
-      <section>
-        <SectionSubmit />
-      </section>
-      <Footer />
-    </Scroll>
+    <section id='home' name='home'>
+      <HeroSection />
+    </section>
+    <section>
+      <Sponsor />
+    </section>
+    <section id='what' name='what'>
+      <WhatSection />
+    </section>
+    <section id='who' name='who'>
+      <MainSession />
+    </section>
+    <section id='where' name='where'>
+      <Schedule />
+    </section>
+    <section>
+      <SectionSubmit />
+    </section>
+    <Footer />
   </Fragment>
 )
