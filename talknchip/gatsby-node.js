86943fd77607aaf30be7f63c345e9f43a8286a04
Keerati <keerati.jear@gmail.com>
2018-08-24 03:23:58 +0700

    [FIX] fix to build and start

---
+++ talknchip/gatsby-node.js
@@ -0,0 +1,6 @@
+exports.modifyBabelrc = ({ babelrc }) => ({
+  ...babelrc,
+  ...process.env.NODE_ENV !== "development" && {
+    plugins: babelrc.plugins.concat(["transform-regenerator", "transform-runtime"])
+  }
+})
