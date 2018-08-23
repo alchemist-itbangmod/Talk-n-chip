3d3b43496ea0bd9b0e1c8dc72f610a5c488ddb76
Keerati <keerati.jear@gmail.com>
2018-08-23 23:51:52 +0700

    [UPDATE] Update button style

--- talknchip/src/components/core/Button.js
+++ talknchip/src/components/core/Button.js
@@ -15,6 +15,24 @@ const Button = styled(DefaultButton)`
       text-decoration: none;
       box-shadow: none !important;
     }
+
+  @media (max-width: 768px) {
+    font-size: 18px;
+  }
+`
+
+export const ButtonOutline = styled(Button)`
+    background-color: transparent;
+    color: #69302c;
+    border: 1px solid #69302c;
+
+
+    &:hover, &:active {
+      color: #fff;
+      background-color: #421714 !important;
+      text-decoration: none;
+      box-shadow: none !important;
+    }
 `
 
 export default Button
