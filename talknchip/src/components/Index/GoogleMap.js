b945837f15283a3a37f0c682d47c9946d9b66771
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-08-23 17:01:40 +0700

    [UPDATE] remove MapImage styled

--- talknchip/src/components/Index/GoogleMap.js
+++ talknchip/src/components/Index/GoogleMap.js
@@ -1,17 +1,8 @@
 import React, { Component } from "react"
-import styled from "styled-components"
 import { withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
 class Map extends Component {
   render () {
-    const MapImage = styled.div`
-      max-width: 300px;
-    height: auto;
-    border-radius: 2%;
-    @media (max-width: 768px) {
-    max-width: 150px;
-    }
-  `
     const MapLocation = withGoogleMap(props => (
       <GoogleMap
         defaultCenter={{ lat: 13.730882, lng: 100.781345 }}
@@ -21,7 +12,7 @@ class Map extends Component {
       </GoogleMap>
     ))
     return (
-      <div >
+      <div>
         <MapLocation
           containerElement={<div style={{ height: "300px", width: "300px" }} />}
           mapElement={<div style={{ height: "100%" }} />}
